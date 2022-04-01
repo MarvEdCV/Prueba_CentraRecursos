d15633eff64712a8de3ee0ec63089b4c76d6a3cf
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-04-01 14:28:20 -0600

    Update app-routing.module.ts
    Agregamos las 3 rutas que nos serviran para Mostrar, Agregar y Actualizar a los empleados.

--- ControlCOVID/src/app/app-routing.module.ts
+++ ControlCOVID/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActualizarComponent } from './Empleado/actualizar/actualizar.component';
+import { AgregarComponent } from './Empleado/agregar/agregar.component';
+import { MostrarComponent } from './Empleado/mostrar/mostrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path:'Mostrar',component:MostrarComponent
+},
+{
+path:'Agregar',component:AgregarComponent
+},
+{
+path:'Actualizar',component:ActualizarComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
