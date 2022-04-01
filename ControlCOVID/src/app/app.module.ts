eac0434ada3402f0af754bd7205b9f4a2eed20b8
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-04-01 14:29:49 -0600

    Component generation
    Generamos los 3 componetes que de momento se quieren utilizar por medio de las instrucciones de angular.

--- ControlCOVID/src/app/app.module.ts
+++ ControlCOVID/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MostrarComponent } from './Empleado/mostrar/mostrar.component';
+import { AgregarComponent } from './Empleado/agregar/agregar.component';
+import { ActualizarComponent } from './Empleado/actualizar/actualizar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MostrarComponent,
+    AgregarComponent,
+    ActualizarComponent
   ],
   imports: [
     BrowserModule,
