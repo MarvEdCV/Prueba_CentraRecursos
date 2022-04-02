0c5b0193e22846fd68f88e80cf0c188ded66a7b1
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-04-02 15:51:44 -0600

    Update app.module.ts
    Agregamos los diferentes imports para el proyecto, para manejar peticiones y para el manejo de formularios.

--- ControlCOVID/src/app/app.module.ts
+++ ControlCOVID/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { MostrarComponent } from './Empleado/mostrar/mostrar.component';
 import { AgregarComponent } from './Empleado/agregar/agregar.component';
 import { ActualizarComponent } from './Empleado/actualizar/actualizar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { ActualizarComponent } from './Empleado/actualizar/actualizar.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
