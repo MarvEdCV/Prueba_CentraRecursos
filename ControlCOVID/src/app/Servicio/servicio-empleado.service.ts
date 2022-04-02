37a87167153560093aabc41423118f62d56ed889
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-04-02 15:53:34 -0600

    Update servicio-empleado.service.ts
    Creamos todos los servicios necesarios para poder consumir la api por medio de las urls establecidas en el controlador del backend

--- ControlCOVID/src/app/Servicio/servicio-empleado.service.ts
+++ ControlCOVID/src/app/Servicio/servicio-empleado.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
+import { Empleado } from '../Modelo/Empleado'; 
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioEmpleadoService {
 
-  constructor() { }
+  constructor(private http: HttpClient ) { }
+  private MyUrlBackend = 'http://localhost:8080/api/v1/empleados'; //Ruta para consumir la API creada en Srpring boot Java para consumir todos los empleados
+
+  ObtenerEmpleados():Observable<Empleado[]>{
+    return this.http.get<Empleado[]>(`${this.MyUrlBackend}`);
+  }
+  RegistrarEmpleado(empleado:Empleado):Observable<Object>{
+    return this.http.post(`${this.MyUrlBackend}`,empleado);
+  }
+  obtenerEmpleadoPorId(id:number):Observable<Empleado>{
+    return this.http.get<Empleado>(`${this.MyUrlBackend}/${id}`);
+  }
+   //este metodo sirve para actualizar el empleado
+   actualizarEmpleado(id:number,empleado:Empleado) : Observable<Object>{
+    return this.http.put(`${this.MyUrlBackend}/${id}`,empleado);
+  }
+  eliminarEmpleado(id:number): Observable<Object>{
+    return this.http.delete(`${this.MyUrlBackend}/${id}`)
+  }
 }
